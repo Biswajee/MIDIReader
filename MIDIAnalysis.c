74c85da10beb50ed52f07453041a2ec6cf89e21b
Biswajee <roy.biswajeet161@gmail.com>
2020-10-06 12:37:19 +0530

    Updates

--- MIDIAnalysis.c
+++ MIDIAnalysis.c
@@ -9,30 +9,46 @@
 
 struct headerChunk {
     char fileID[4];     // indicates an actual MiDi file
-    int length;         // header length
+    short length;       // header length
     short format;       // format [2 bytes]   0 : single track | 1 : multiple track | 2 : multiple song file format
     short n;            // number of tracks that follow
     short division;     // unit of time for delta timing
 };
 
-struct trackChunk {
+struct trackEvent {
+    short vTime;        // a variable length value specifying the elapsed time (delta time) from the previous event to this event
+    short midiEvent;    // any MIDI channel message such as note-on or note-off
+};
 
+struct trackChunk {
+    char trackID[4];    // marks the beginning of a track
+    int length;         // track length
+    struct trackEvent event;   // track event structure
 };
 
 int main() {
     FILE *fp;
-    char data;
+    struct headerChunk head;
+    struct trackChunk track;
+    struct trackEvent evt;
+
     fp = fopen("sample.mid", "rb");
-    int i=0, j=0;
     
     if(fp == NULL) {
         printf("\nError: failed to open mid file\nTerminating...\n");
         return -1;
     }
-    fread(&data, 1, sizeof(unsigned char), fp);
-
-    printf("\nPrinting data (i = %d)...\n\n", i);
+    fread(&head.fileID[0], 1, sizeof(unsigned char), fp);
+    fread(&head.fileID[1], 1, sizeof(unsigned char), fp);
+    fread(&head.fileID[2], 1, sizeof(unsigned char), fp);
+    fread(&head.fileID[3], 1, sizeof(unsigned char), fp);
+    fread(&head.length, 1, sizeof(head.length), fp);
+    fread(&head.format, 1, sizeof(head.format), fp);
+    fread(&head.n, 1, sizeof(head.n), fp);
+    fread(&head.division, 1, sizeof(head.division), fp);
     
-    printf("%c ", data);
+    printf("File pointer position: %ld", ftell(fp));
+    
+    printf("\nMIDI Headers:\nFile Descriptor : %s\nLength : %d\nFormat : %d\nN : %d\nDivision : %d", head.fileID, head.length, head.format, head.n, head.division);
     return 0;
 }
